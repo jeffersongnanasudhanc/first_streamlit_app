ad50c9ac949af00e10bcb957bf3b56f3e5b70b9b
jeffersongnanasudhanc <129780196+jeffersongnanasudhanc@users.noreply.github.com>
2023-04-03 22:49:20 +0530

    Update streamlit_app.py
    Fixing typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streatlit.text('🥑🍞 Avocado Toast')
-
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
