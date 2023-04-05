2de670ace04000cc218a913de96c82e22306c04b
jeffersongnanasudhanc <129780196+jeffersongnanasudhanc@users.noreply.github.com>
2023-04-05 11:05:07 +0530

    Update streamlit_app.py
    Setting control flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
+import urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -32,6 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Store in dataframe and display
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
